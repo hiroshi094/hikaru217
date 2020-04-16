c3187436721982c38f8cf5a64d41a628c2fbcb55
hiroshi <hikaru217@gmail.com>
2020-04-16 22:21:26 +0900

    #22 スタイルの適用

---
+++ tools/components/common_header.php
@@ -0,0 +1,10 @@
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+<link
+    rel="stylesheet"
+    type="text/css"
+    href="css/tools_common.css?<?php echo date('YmdGis', filemtime('css/tools_common.css')); ?>">
+<link rel="icon" href="hikaru217.ico">
--- tools/show_tables.php
+++ tools/show_tables.php
@@ -3,8 +3,6 @@ require_once('models/DbConnection.php');
 
 use tools\DbConnection as DbConnection;
 
-echo '<h1>テーブル一覧</h1>';
-
 $dbConnection = new DbConnection();
 if ($dbConnection === false) {
     echo '<a href="db_connection.php">データベースに接続してください</a>';
@@ -16,19 +14,30 @@ if (empty($tableInfo)) {
     echo 'テーブルがありません';
 }
 ?>
-<table>
-    <tr>
-        <th>#</th>
-        <th>table name</th>
-        <th>table comment</th>
-    </tr>
-    <?php $seq=1; ?>
-    <?php foreach ($tableInfo as $table) : ?>
-        <tr>
-            <td><?php echo $seq; ?></td>
-            <td><?php echo $table['TABLE_NAME']; ?></td>
-            <td><?php echo $table['TABLE_COMMENT']; ?></td>
-        </tr>
-        <?php $seq++;?>
-    <?php endforeach; ?>
-</table>
+<?php include_once('components/common_header.php'); ?>
+<link rel="stylesheet" type="text/css" href="css/show_table.css?<?php echo date('YmdGis', filemtime('css/show_table.css')); ?>">
+<h1>テーブル一覧</h1>
+<div class="tableInfo">
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>table name</th>
+                <th>table comment</th>
+                <th>action</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php $seq=1; ?>
+            <?php foreach ($tableInfo as $table) : ?>
+                <tr>
+                    <td><?php echo $seq; ?></td>
+                    <td><?php echo $table['TABLE_NAME']; ?></td>
+                    <td><?php echo $table['TABLE_COMMENT']; ?></td>
+                    <td><i class="material-icons fsize20">edit</i><i class="material-icons fsize20">description</i></td>
+                </tr>
+                <?php $seq++;?>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
